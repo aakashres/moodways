0da777d373912d197809845225d68733244f6e0d
Aakash Shrestha <aakash.shres@gmail.com>
2017-06-26 16:14:43 +0545

    Model Desing Phase II

--- website/website/models.py
+++ website/website/models.py
@@ -18,14 +18,35 @@ class Timestampable(models.Model):
         super().save()
 
 
+class Photo(Timestampable):
+    photo = models.ImageField()
+    caption = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.pk
+
+
+class Gallery(Timestampable):
+    name = models.CharField(max_length=255)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Package(Timestampable):
     title = models.CharField(max_length=1024)
     slug = models.SlugField(unique=True)
     subTitle = models.CharField(max_length=1024, null=True, blank=True)
     titlePic = models.ImageField(null=True, blank=True)
+    caption = models.CharField(max_length=255, null=True, blank=True)
     body = RichTextField()
-    price = models.FloatField(default=0)
-    discount = models.FloatField(default=0)
+    price = models.FloatField(default=0, null=True, blank=True)
+    discount = models.FloatField(default=0, null=True, blank=True)
+    gallery = models.OneToOneField(Gallery, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Place(Timestampable):
@@ -53,7 +74,41 @@ class Season(Timestampable):
 
 
 class Itenary(Timestampable):
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
     day = models.CharField(max_length=255)
     description = RichTextField()
 
-    return
+    def __str__(self):
+        return self.name
+
+
+class Blog(Timestampable):
+    author = models.CharField(max_length=255, null=True, blank=True)
+    authorPic = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=255)
+    subTitle = models.CharField(max_length=255, null=True, blank=True)
+    titlePic = models.ImageField(null=True, blank=True)
+    caption = models.CharField(max_length=255, null=True, blank=True)
+    body = RichTextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(Timestampable):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(Timestampable):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    body = RichTextField()
+
+    def __str__(self):
+        return self.title
