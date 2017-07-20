7d37a607329bc9aa0fc89db28e426cb6a904340e
Manish Sharma <mks2064@gmail.com>
2017-07-20 09:37:17 +0545

    ch

--- website/moodways/settings.py
+++ website/moodways/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "root", "static_cdn")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "root", "media_cdn")
+MEDIA_ROOT = 'media'
 
 # Django Ckeditor
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
